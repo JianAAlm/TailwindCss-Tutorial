edf27ecbe08a80021bb387c7ceadcf9c0438fb2d
Jeannie Abrantes Almeida <jeannie.almeida@compasso.com.br>
2021-12-08 09:15:34 -0300

    tutorial done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-4">
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div>
+          <div className="text-xl font-medium text-primary">ChitChat</div>
+          <p className="text-blue-450">You have a new message!</p>
+        </div> 
+      </div>
+      <TailwindCssButton>Tailwind Button Test</TailwindCssButton>
     </div>
+  );
+}
+
+function TailwindCssButton(props){
+  return(
+    <button className="bg-blue-500 text-white font-medium px-4 py-2 rounded-md hover:bg-blue-600 "> {props.children}</button>
   );
 }
 
